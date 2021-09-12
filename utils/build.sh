1fe546fbef260fdcc9ae7b328551ba93fefff37d
Mason Ginter <mason@dagint.com>
2021-09-12 18:19:03 -0400

    Add opencv_contrib modules and update version
    Need to update version in order to support opencv_contrib
    (https://github.com/opencv/opencv_contrib/issues/2526)

--- utils/build.sh
+++ utils/build.sh
@@ -1,12 +1,18 @@
-git clone --branch 4.3.0 --depth 1 https://github.com/opencv/opencv.git
-
+# Grab git source
+(
+    git clone --branch 4.5.3 --depth 1 https://github.com/opencv/opencv.git
+    cd opencv
+    git clone --branch 4.5.3 --depth 1 https://github.com/opencv/opencv_contrib.git
+)
 # Build
 (
-    cd opencv &&
-    git checkout 4.3.0 &&
-
+    cd ./opencv/opencv_contrib &&
+    git checkout 4.5.3 &&
+    cd ../ &&
+    git checkout 4.5.3 &&
+    ls &&
     # Add non async flag before compiling in the python build_js.py script
-    docker run --rm --workdir /code -v "$PWD":/code "trzeci/emscripten:sdk-tag-1.39.4-64bit" python ./platforms/js/build_js.py build_wasm --build_wasm --build_test --build_flags "-s WASM=1 -s WASM_ASYNC_COMPILATION=0 -s SINGLE_FILE=0 "
+    docker run --rm --workdir /code -v "$PWD":/code "trzeci/emscripten:sdk-tag-1.39.4-64bit" python ./platforms/js/build_js.py build_wasm --build_wasm --build_test --cmake_option="-DOPENCV_EXTRA_MODULES_PATH=/code/opencv_contrib/modules" --build_flags "-s WASM=1 -s WASM_ASYNC_COMPILATION=0 -s SINGLE_FILE=0 "
 )
 
 # Copy compilation result
